07b570b1d155a7f379236935988e11208719fdea
Franco Oyarzo Calisto <foyarzo2023@alu.uct.cl>
2025-06-15 04:25:25 -0400

    se agrega frontend simple sin funcionalidad extraordinaria que busca empresa por rut

--- frontend-lafactoria/src/App.js
+++ frontend-lafactoria/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import EmpresaForm from './components/EmpresaForm/page.jsx';
 import FacturaForm from './components/FacturaForm/page.jsx';
+import BuscarEmpresa from './components/BuscarEmpresa/page.jsx';
 
 function App() {
   return (
@@ -12,12 +13,16 @@ function App() {
         <nav className="mb-4">
           <Link to="/empresas" className="btn btn-outline-primary me-2">Empresas</Link>
           <Link to="/facturas" className="btn btn-outline-success">Facturas</Link>
+          <Link to="/buscar" className="btn btn-outline-secondary">Buscar Empresa</Link>
         </nav>
 
         <Routes>
           <Route path="/" element={<div className="text-muted"><p>Bienvenido al sistema de facturación.</p></div>} />
           <Route path="/empresas" element={<EmpresaForm />} />
           <Route path="/facturas" element={<FacturaForm />} />
+          <Route path="/buscar" element={<BuscarEmpresa />} />
+
+
         </Routes>
       </div>
     </Router>
