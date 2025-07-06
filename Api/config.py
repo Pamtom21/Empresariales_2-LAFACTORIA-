19954505a25c50ccf1bba6c0d20f92f80f519a16
Pamtom21 <joluis.2.11.04@gmail.com>
2025-07-05 22:43:51 -0400

    correccion errores

--- Api/config.py
+++ Api/config.py
@@ -7,3 +7,6 @@ load_dotenv()  # Carga el archivo .env
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+        # Configuración del JWT
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'una_clave_secreta_segura')  # Usa un valor seguro
+    JWT_ACCESS_TOKEN_EXPIRES = 3600  # Tiempo de expiración del token (en segundos, 1 hora)
